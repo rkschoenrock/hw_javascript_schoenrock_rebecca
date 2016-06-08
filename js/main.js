4a277545790a95abd86f442dd8a85ac7d7cfdd00
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-06-08 17:54:59 -0400

    Add five objects to Main JavaScript file

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var barackObama = {
+	firstName: 'Barack',
+	lastName: 'Obama',
+	termLength: '8 years',
+	party: 'Democrat',
+	yearsOfPresidency: '2009-2017'
+};
+var georgeWBush = {
+	firstName: 'George',
+	lastName: 'Bush',
+	termLength: '8 years',
+	party: 'Republican',
+	yearsOfPresidency: '2001-2009'
+};
+var billClinton = {
+	firstName: 'Bill',
+	lastName: 'Clinton',
+	termLength: '8 years',
+	party: 'Democrat',
+	yearsOfPresidency: '1993-2001'
+};
+var georgeHWBush = {
+	firstName: 'George',
+	lastName: 'Bush',
+	termLength: '4 years',
+	party: 'Democrat',
+	yearsOfPresidency: '1989-1993'
+};
+var ronaldReagan = {
+	firstName: 'Ronald',
+	lastName: 'Reagan',
+	termLength: '8 years',
+	party: 'Republican',
+	yearsOfPresidency: '1981-1989'
+};
