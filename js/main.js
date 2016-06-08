46dc6e2701a8c3079bd18ffe6b4e85b3afdb499f
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-06-08 18:17:34 -0400

    Create array from five objects

--- js/main.js
+++ js/main.js
@@ -1,35 +1,8 @@
-var barackObama = {
-	firstName: 'Barack',
-	lastName: 'Obama',
-	termLength: '8 years',
-	party: 'Democrat',
-	yearsOfPresidency: '2009-2017'
-};
-var georgeWBush = {
-	firstName: 'George',
-	lastName: 'Bush',
-	termLength: '8 years',
-	party: 'Republican',
-	yearsOfPresidency: '2001-2009'
-};
-var billClinton = {
-	firstName: 'Bill',
-	lastName: 'Clinton',
-	termLength: '8 years',
-	party: 'Democrat',
-	yearsOfPresidency: '1993-2001'
-};
-var georgeHWBush = {
-	firstName: 'George',
-	lastName: 'Bush',
-	termLength: '4 years',
-	party: 'Repiblican',
-	yearsOfPresidency: '1989-1993'
-};
-var ronaldReagan = {
-	firstName: 'Ronald',
-	lastName: 'Reagan',
-	termLength: '8 years',
-	party: 'Republican',
-	yearsOfPresidency: '1981-1989'
-};
+var presidents = [
+	{firstName: 'Barack', lastName: 'Obama', termLength: '8 years', party: 'Democrat', yearsOfPresidency: '2009-2017'},
+	{firstName: 'George', lastName: 'Bush', termLength: '8 years', party: 'Republican', yearsOfPresidency: '2001-2009'},
+	{firstName: 'Bill', lastName: 'Clinton', termLength: '8 years', party: 'Democrat', yearsOfPresidency: '1993-2001'},
+	{firstName: 'George', lastName: 'Bush', termLength: '4 years', party: 'Republican', yearsOfPresidency: '1989-1993'},
+	{firstName: 'Ronald', lastName: 'Reagan', termLength: '8 years', party: 'Republican', yearsOfPresidency: '1981-1989'}
+];
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
